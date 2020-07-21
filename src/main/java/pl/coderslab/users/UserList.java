51dfe7d51f86d36014668017373e2a54303243fc
Roman <roman.paszkow@gmail.com>
2020-07-21 23:27:33 +0200

    21.07.20

--- src/main/java/pl/coderslab/users/UserList.java
+++ src/main/java/pl/coderslab/users/UserList.java
@@ -1,18 +1,40 @@
 package pl.coderslab.users;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import pl.coderslab.dao.UserDao;
+import pl.coderslab.entity.User;
+import pl.coderslab.utils.DbUtil;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Arrays;
 
 
 @WebServlet("/user/list")
 public class UserList extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public static final Logger log = LogManager.getLogger(UserList.class);
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try(Connection connection = DbUtil.getConnection()) {
+            UserDao userDao = new UserDao();
+            User[]currentUsers = userDao.findAll();
+            log.debug(Arrays.toString(currentUsers));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
         getServletContext().getRequestDispatcher("/users/list.jsp")
                 .forward(request, response);
+
 
     }
 
